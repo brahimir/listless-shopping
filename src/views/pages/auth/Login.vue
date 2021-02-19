d90295dd0a1a527b630591666c18e924155bbf30
brahimir <brahimir.nick@gmail.com>
2021-02-19 15:20:14 -0500

    Added login and register pages using vuelidate validation

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,32 +1,61 @@
 <style lang="scss" scoped></style>
 
 <template>
-  <div id="register">
+  <div id="login">
     <h2 class="text-center mb-10">sign in</h2>
 
-    <v-form>
+    <v-form @submit.stop.prevent="login" v-model="valid">
       <v-container>
         <v-row>
-          <v-col cols="12" xl="4">
-            <v-text-field v-model="email" :rules="emailRules" label="e-mail" required></v-text-field>
-          </v-col>
-
-          <v-col cols="12" xl="4">
+          <v-col cols="12" md="4">
             <v-text-field
-              v-model="password"
-              :rules="passwordRules"
-              label="password"
+              v-model="email"
+              :error-messages="emailErrors"
+              label="email"
               required
-              type="password"
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
             ></v-text-field>
           </v-col>
 
-          <v-col class="mt-5" cols="12" xl="4">
-            <v-btn tile block elevation="2" large>login</v-btn>
+          <v-col cols="12" md="4">
+            <v-text-field
+              type="password"
+              v-model="password"
+              :error-messages="passwordErrors"
+              label="password"
+              required
+              @input="$v.password.$touch()"
+              @blur="$v.password.$touch()"
+            ></v-text-field>
           </v-col>
 
-          <v-col class="text-center mt-5" cols="12" xl="4">
-            <v-label><router-link to="/register">don't have an account?</router-link></v-label>
+          <v-col class="mt-5" cols="12" md="4">
+            <v-btn
+              tile
+              block
+              elevation="2"
+              large
+              :loading="isLoading"
+              :disabled="isLoading || $v.$invalid"
+              color="success"
+              @click="login"
+            >
+              sign in
+            </v-btn>
+          </v-col>
+        </v-row>
+
+        <v-row class="mt-10">
+          <v-col cols="12" md="4">
+            <!-- start:: Login Failure Message -->
+            <v-card v-if="errorLogin" elevation="17" tile color="danger">
+              <v-card-title class="justify-center">uh oh!</v-card-title>
+              <v-card-text class="text-center">
+                invalid username or password. please try again.
+              </v-card-text>
+            </v-card>
+            <!-- start:: Login Failure Message -->
           </v-col>
         </v-row>
       </v-container>
@@ -40,30 +69,73 @@ import Vue from "vue";
 import { User } from "../../../core/_models/user.model";
 // Services
 import AuthService from "../../../core/services/auth.service";
-import FiltersService from "../../../core/services/filters.service";
-// Components
-import Spinner from "../../components/content/Spinner.vue";
+// Vuelidate
+import { validationMixin } from "vuelidate";
+import { email, required } from "vuelidate/lib/validators";
 
 export default Vue.extend({
-  // components: { Spinner },
-  name: "Login",
+  mixins: [validationMixin],
+  name: "Register",
 
   data: () => ({
+    isLoading: false,
+    errorLogin: false,
+    message: "",
+    valid: false,
     password: "",
-    passwordRules: [(v: any) => !!v || "enter your password"],
-    email: "",
-    emailRules: [
-      (v: any) => !!v || "enter your e-mail",
-      (v: any) => /.+@.+/.test(v) || "enter a valid e-mail"
-    ]
+    email: ""
   }),
 
-  methods: {},
+  validations: {
+    email: { required, email },
+    password: { required }
+  },
 
-  filters: {
-    lowercase: FiltersService.lowercase
+  methods: {
+    login: function(): void {
+      // Check form for errors.
+      this.$v.$touch();
+      if (this.$v.$anyError) return;
+
+      const credentials: any = {
+        email: this.email,
+        password: this.password
+      };
+
+      this.isLoading = true;
+      Promise.resolve(AuthService.login(credentials)).then(data => {
+        if (data) {
+          const user: User = data;
+          console.log(user);
+          this.isLoading = false;
+        } else {
+          this.errorLogin = true;
+          this.isLoading = false;
+        }
+      });
+    },
+
+    clearForm: function(): void {
+      this.$v.$reset();
+      this.password = "";
+      this.email = "";
+    }
+  },
+
+  computed: {
+    passwordErrors() {
+      const errors: any[] = [];
+      if (!this.$v.$dirty) return errors;
+      !this.$v.password.required && errors.push("enter your password");
+      return errors;
+    },
+    emailErrors() {
+      const errors: any[] = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("enter a valid email");
+      !this.$v.email.required && errors.push("enter your email");
+      return errors;
+    }
   }
-
-  // mounted() {}
 });
 </script>
--- src/views/pages/auth/Register.vue
+++ src/views/pages/auth/Register.vue
@@ -9,36 +9,78 @@
   <div id="register">
     <h2 class="text-center mb-10">register</h2>
 
-    <v-form v-model="valid">
+    <v-form @submit.stop.prevent="register" v-model="valid">
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
-            <v-text-field v-model="email" :rules="emailRules" label="e-mail" required></v-text-field>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="email"
+              required
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+            ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="4">
             <v-text-field
+              type="password"
               v-model="password"
-              :rules="passwordRules"
+              :error-messages="passwordErrors"
               label="password"
               required
-              type="password"
+              @input="$v.password.$touch()"
+              @blur="$v.password.$touch()"
             ></v-text-field>
           </v-col>
 
           <v-col class="mt-5" cols="12" md="4">
-            <v-btn tile block elevation="2" large>create account</v-btn>
+            <v-btn
+              tile
+              block
+              elevation="2"
+              large
+              :loading="isLoading"
+              :disabled="isLoading || $v.$invalid"
+              color="success"
+              @click="register"
+            >
+              create account
+            </v-btn>
           </v-col>
         </v-row>
 
         <v-row class="mt-10">
           <v-col cols="12" md="4">
-            <v-card elevation="17" tile color="red">
+            <!-- start:: Register Success Message  -->
+            <v-card v-if="successRegister" elevation="17" tile color="success">
+              <v-card-title class="justify-center">successfully registered!</v-card-title>
+              <v-card-text class="text-center">
+                <router-link to="/login">click here to log in.</router-link>
+              </v-card-text>
+            </v-card>
+            <!-- end:: Register Success Message -->
+
+            <!-- start:: Register Failure Message -->
+            <v-card v-if="errorRegister" elevation="17" tile color="danger">
+              <v-card-title class="justify-center">uh oh!</v-card-title>
+              <v-card-text class="text-center">
+                there was a problem registering your account. please try again.
+              </v-card-text>
+            </v-card>
+            <!-- start:: Register Failure Message -->
+          </v-col>
+        </v-row>
+
+        <v-row class="mt-10">
+          <v-col cols="12" md="4">
+            <v-card elevation="17" tile color="orange darken-3">
               <v-card-title class="justify-center">PLEASE READ</v-card-title>
-              <v-card-text class="text-center"
-                >This web application is currently IN DEVELOPMENT. When registering, DO NOT use personal
-                data or credentials. Thank you.</v-card-text
-              >
+              <v-card-text class="text-center">
+                this web application is currently IN DEVELOPMENT. when registering, DO NOT use personal
+                data or credentials. thank you.
+              </v-card-text>
             </v-card>
           </v-col>
         </v-row>
@@ -53,34 +95,78 @@ import Vue from "vue";
 import { User } from "../../../core/_models/user.model";
 // Services
 import AuthService from "../../../core/services/auth.service";
-import FiltersService from "../../../core/services/filters.service";
-// Components
-import Spinner from "../../components/content/Spinner.vue";
+// Vuelidate
+import { validationMixin } from "vuelidate";
+import { email, required, minLength } from "vuelidate/lib/validators";
 
 export default Vue.extend({
-  // components: { Spinner },
+  mixins: [validationMixin],
   name: "Register",
 
   data: () => ({
+    isLoading: false,
+    errorRegister: false,
+    successRegister: false,
+    message: "",
     valid: false,
     password: "",
-    passwordRules: [
-      (v: any) => !!v || "password is required",
-      (v: any) => v.length >= 8 || "password must be at least 8 characters long."
-    ],
-    email: "",
-    emailRules: [
-      (v: any) => !!v || "e-mail is required",
-      (v: any) => /.+@.+/.test(v) || "enter a valid e-mail"
-    ]
+    email: ""
   }),
 
-  methods: {},
+  validations: {
+    email: { required, email },
+    password: { required, minLength: minLength(8) }
+  },
 
-  filters: {
-    lowercase: FiltersService.lowercase
+  methods: {
+    // todo - login the user if successfully registered, instead of routing them to login page.
+    register: function(): void {
+      // Check form for errors.
+      this.$v.$touch();
+      if (this.$v.$anyError) return;
+
+      const user: User = {
+        email: this.email,
+        password: this.password,
+        settings: [],
+        profile: []
+      };
+
+      this.isLoading = true;
+      Promise.resolve(AuthService.register(user)).then(data => {
+        if (data) {
+          this.successRegister = true;
+          this.isLoading = false;
+          this.clearForm();
+        } else {
+          this.errorRegister = true;
+          this.isLoading = false;
+        }
+      });
+    },
+
+    clearForm: function(): void {
+      this.$v.$reset();
+      this.password = "";
+      this.email = "";
+    }
+  },
+
+  computed: {
+    passwordErrors() {
+      const errors: any[] = [];
+      if (!this.$v.$dirty) return errors;
+      !this.$v.password.minLength && errors.push("password must be at least 8 characters long");
+      !this.$v.password.required && errors.push("enter your password");
+      return errors;
+    },
+    emailErrors() {
+      const errors: any[] = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("enter a valid email");
+      !this.$v.email.required && errors.push("enter your email");
+      return errors;
+    }
   }
-
-  // mounted() {}
 });
 </script>
