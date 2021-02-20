8302a3d9580579c56fdf75a912afa83a12047719
brahimir <brahimir.nick@gmail.com>
2021-02-20 11:38:56 -0500

    Updated User Model to have created at and updated at properties.

--- functions/app/models/auth/user.model.js
+++ functions/app/models/auth/user.model.js
@@ -7,30 +7,29 @@ var mongoose = require("mongoose"),
 /**
  * User Schema
  */
-var UserSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    required: true
+var UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true
+    },
+    hash_password: {
+      type: String
+    },
+    settings: {
+      type: Object,
+      required: true
+    },
+    profile: {
+      type: Object,
+      required: true
+    }
   },
-  hash_password: {
-    type: String
-  },
-  settings: {
-    type: Object,
-    required: true
-  },
-  profile: {
-    type: Object,
-    required: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 UserSchema.methods.comparePassword = function(password) {
   return bcrypt.compareSync(password, this.hash_password);
