61304bcd421d8aed1e3357b373a536b6939d0252
brahimir <brahimir.nick@gmail.com>
2021-03-04 12:53:28 -0500

    Adding a ListCard.

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,10 +1,14 @@
+<style lang="scss" scoped></style>
+
 <template>
   <div></div>
 </template>
 
 <script>
+// Vue
 import Vue from "vue";
+// Moment
+import moment from "moment";
+
 export default Vue.extend({});
 </script>
-
-<style lang="scss" scoped></style>
